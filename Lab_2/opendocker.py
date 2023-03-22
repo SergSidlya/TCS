6fb287e17fe14028bebb0d13178d4755e7a35419
SergSidlya <127301398+SergSidlya@users.noreply.github.com>
2023-03-22 21:57:54 +0200

    Update opendocker.py

--- Lab_2/opendocker.py
+++ Lab_2/opendocker.py
@@ -1,12 +1,9 @@
 import docx
 import os
 
-# Визначаємо шлях до файлу
 file_path = os.path.join(os.getcwd(), 'Docker Basics.docx')
 
-# Відкриваємо файл
 document = docx.Document(file_path)
 
-# Виводимо текст документу в термінал
 for paragraph in document.paragraphs:
     print(paragraph.text)
