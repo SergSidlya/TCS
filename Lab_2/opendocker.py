59415d3477db660b53ed3548775da5c4c9fe2b5f
SergSidlya <127301398+SergSidlya@users.noreply.github.com>
2023-03-24 11:48:53 +0200

    Delete Lab_2 directory

--- Lab_2/opendocker.py
+++
@@ -1,9 +0,0 @@
-import docx
-import os
-
-file_path = os.path.join(os.getcwd(), 'Docker Basics.docx')
-
-document = docx.Document(file_path)
-
-for paragraph in document.paragraphs:
-    print(paragraph.text)
